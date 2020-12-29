c986fa9045bc4de3434539fdbe91a3ba7d9c0afd
MyTh1c2L <75614334+MyTh1c2L@users.noreply.github.com>
2020-12-29 16:58:36 +0400

    В качестве базы данных теперь выступает JSON

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace TechnicalSupport
+namespace TechnicalSupport
 {
     partial class MainForm
     {
@@ -40,21 +40,24 @@
             this.ProblemButton = new System.Windows.Forms.Button();
             this.TarifButton = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.dbproblem = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.dbtarif = new System.Windows.Forms.Button();
+            this.phoneTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BDOpen = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.menuStrip1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.справкаToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
@@ -74,8 +77,11 @@
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(552, 266);
+            this.label1.Location = new System.Drawing.Point(106, 77);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(207, 16);
             this.label1.TabIndex = 1;
@@ -83,10 +89,13 @@
             // 
             // SpeedLabel
             // 
+            this.SpeedLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.SpeedLabel.AutoSize = true;
             this.SpeedLabel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.SpeedLabel.ForeColor = System.Drawing.Color.Blue;
-            this.SpeedLabel.Location = new System.Drawing.Point(606, 292);
+            this.SpeedLabel.Location = new System.Drawing.Point(158, 93);
             this.SpeedLabel.Name = "SpeedLabel";
             this.SpeedLabel.Size = new System.Drawing.Size(98, 19);
             this.SpeedLabel.TabIndex = 2;
@@ -99,8 +108,10 @@
             // 
             // label2
             // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(93, 19);
+            this.label2.Location = new System.Drawing.Point(112, 19);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(214, 16);
             this.label2.TabIndex = 3;
@@ -109,7 +120,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(119, 19);
+            this.label3.Location = new System.Drawing.Point(113, 21);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(205, 16);
             this.label3.TabIndex = 4;
@@ -117,130 +128,129 @@
             // 
             // ProblemButton
             // 
-            this.ProblemButton.Location = new System.Drawing.Point(7, 40);
+            this.ProblemButton.Location = new System.Drawing.Point(6, 40);
             this.ProblemButton.Name = "ProblemButton";
-            this.ProblemButton.Size = new System.Drawing.Size(400, 81);
+            this.ProblemButton.Size = new System.Drawing.Size(402, 81);
             this.ProblemButton.TabIndex = 5;
-            this.ProblemButton.Text = "Нажми на меня!";
+            this.ProblemButton.Text = "Решить проблему";
             this.ProblemButton.UseVisualStyleBackColor = true;
             this.ProblemButton.Click += new System.EventHandler(this.ProblemButton_Click);
             // 
             // TarifButton
             // 
-            this.TarifButton.Location = new System.Drawing.Point(7, 40);
+            this.TarifButton.Location = new System.Drawing.Point(6, 40);
             this.TarifButton.Name = "TarifButton";
-            this.TarifButton.Size = new System.Drawing.Size(400, 81);
+            this.TarifButton.Size = new System.Drawing.Size(405, 81);
             this.TarifButton.TabIndex = 6;
-            this.TarifButton.Text = "Нажми на меня!";
+            this.TarifButton.Text = "Узнать про тарифы";
             this.TarifButton.UseVisualStyleBackColor = true;
             this.TarifButton.Click += new System.EventHandler(this.TarifButton_Click);
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.dbproblem);
-            this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.ProblemButton);
             this.groupBox1.Location = new System.Drawing.Point(12, 28);
+            this.groupBox1.MaximumSize = new System.Drawing.Size(414, 131);
+            this.groupBox1.MinimumSize = new System.Drawing.Size(414, 131);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(414, 195);
+            this.groupBox1.Size = new System.Drawing.Size(414, 131);
             this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
-            // 
-            // dbproblem
-            // 
-            this.dbproblem.Location = new System.Drawing.Point(10, 151);
-            this.dbproblem.Name = "dbproblem";
-            this.dbproblem.Size = new System.Drawing.Size(395, 29);
-            this.dbproblem.TabIndex = 10;
-            this.dbproblem.Text = "Открыть";
-            this.dbproblem.UseVisualStyleBackColor = true;
-            this.dbproblem.Click += new System.EventHandler(this.dbproblem_Click);
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(7, 129);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(149, 16);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Загрузить базу данных:";
             // 
             // groupBox2
             // 
             this.groupBox2.BackColor = System.Drawing.SystemColors.Control;
-            this.groupBox2.Controls.Add(this.dbtarif);
-            this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.label3);
             this.groupBox2.Controls.Add(this.TarifButton);
             this.groupBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.groupBox2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.groupBox2.Location = new System.Drawing.Point(433, 28);
+            this.groupBox2.Location = new System.Drawing.Point(436, 28);
+            this.groupBox2.MaximumSize = new System.Drawing.Size(414, 131);
+            this.groupBox2.MinimumSize = new System.Drawing.Size(414, 131);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(414, 195);
+            this.groupBox2.Size = new System.Drawing.Size(414, 131);
             this.groupBox2.TabIndex = 8;
             this.groupBox2.TabStop = false;
             // 
-            // dbtarif
+            // phoneTextBox
             // 
-            this.dbtarif.Location = new System.Drawing.Point(12, 151);
-            this.dbtarif.Name = "dbtarif";
-            this.dbtarif.Size = new System.Drawing.Size(395, 29);
-            this.dbtarif.TabIndex = 8;
-            this.dbtarif.Text = "Открыть";
-            this.dbtarif.UseVisualStyleBackColor = true;
-            this.dbtarif.Click += new System.EventHandler(this.dbtarif_Click);
+            this.phoneTextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.phoneTextBox.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.phoneTextBox.Location = new System.Drawing.Point(6, 21);
+            this.phoneTextBox.Multiline = true;
+            this.phoneTextBox.Name = "phoneTextBox";
+            this.phoneTextBox.ReadOnly = true;
+            this.phoneTextBox.Size = new System.Drawing.Size(240, 100);
+            this.phoneTextBox.TabIndex = 10;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(252, 21);
+            this.emailTextBox.Multiline = true;
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.ReadOnly = true;
+            this.emailTextBox.Size = new System.Drawing.Size(159, 100);
+            this.emailTextBox.TabIndex = 11;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(8, 129);
+            this.label4.Location = new System.Drawing.Point(130, 18);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(149, 16);
-            this.label4.TabIndex = 7;
+            this.label4.TabIndex = 12;
             this.label4.Text = "Загрузить базу данных:";
             // 
-            // label6
+            // BDOpen
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(13, 232);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(106, 16);
-            this.label6.TabIndex = 9;
-            this.label6.Text = "Обратная связь:";
+            this.BDOpen.Location = new System.Drawing.Point(51, 37);
+            this.BDOpen.Name = "BDOpen";
+            this.BDOpen.Size = new System.Drawing.Size(310, 23);
+            this.BDOpen.TabIndex = 13;
+            this.BDOpen.Text = "Открыть";
+            this.BDOpen.UseVisualStyleBackColor = true;
+            this.BDOpen.Click += new System.EventHandler(this.BDOpen_Click);
             // 
-            // textBox1
+            // groupBox3
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
-            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(13, 252);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(412, 109);
-            this.textBox1.TabIndex = 10;
-            this.textBox1.Text = "Номера справочной службы:\r\n+7 (3412) 553-535\r\n+7 (3412) 553-123\r\n\r\nEmail:\r\ntechsu" +
-    "pport@ya.ru";
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.BDOpen);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.SpeedLabel);
+            this.groupBox3.Location = new System.Drawing.Point(13, 166);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(413, 127);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.emailTextBox);
+            this.groupBox4.Controls.Add(this.phoneTextBox);
+            this.groupBox4.Location = new System.Drawing.Point(436, 166);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(414, 127);
+            this.groupBox4.TabIndex = 15;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Обратная связь";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(860, 377);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label6);
+            this.ClientSize = new System.Drawing.Size(860, 305);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.SpeedLabel);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(876, 416);
-            this.MinimumSize = new System.Drawing.Size(876, 416);
             this.Name = "MainForm";
             this.Text = "Техническая поддержка абонентов";
             this.Load += new System.EventHandler(this.MainForm_Load);
@@ -250,6 +260,10 @@
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -267,13 +281,13 @@
         private System.Windows.Forms.Button ProblemButton;
         private System.Windows.Forms.Button TarifButton;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button dbproblem;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button dbtarif;
+        private System.Windows.Forms.TextBox phoneTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BDOpen;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
     }
 }
 
